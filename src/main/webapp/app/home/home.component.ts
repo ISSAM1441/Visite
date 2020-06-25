759ffe51601c581a7e7724b911389f776d3ef161
ISSAM1441 <issambourne@gmail.com>
2020-06-25 12:22:44 +0100

    map marker & info

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -11,18 +11,42 @@ import { Account } from 'app/core/user/account.model';
   styleUrls: ['home.scss'],
 })
 export class HomeComponent implements OnInit, OnDestroy {
+  /* attt1 = 
+        index: 1,
+        nom: 'الإدارة الجهوية للوكالة الفنية للنقل البري أريانة',
+        lat:36.851422,
+        lng:10.191006
+    
+  ;
+ attt2 = [
+    {
+      index: 2,
+      nom: 'الإدارة الجهوية للوكالة الفنية للنقل البري السيجومي ',
+      latitude:36.781570,
+      longitude:10.115915,
+
+  }
+];*/
+
   account: Account | null = null;
   authSubscription?: Subscription;
   latitude = 36.76402;
   longitude = 10.223848;
-  lat = 36.76402;
-  lng = 10.223848;
+  lat = 36.851422;
+  lng = 10.191006;
+  lat1 = 36.851422;
+  lng1 = 10.191006;
+  lat2 = 36.78157;
+  lng2 = 10.115915;
+  greenMarker = 'http://www.google.com/intl/en_us/mapfiles/ms/micons/green-dot.png';
+  yellowMarker = 'http://www.google.com/intl/en_us/mapfiles/ms/micons/yellow-dot.png';
 
   constructor(private accountService: AccountService, private loginModalService: LoginModalService) {}
 
   onChoseLocation(event: { coords: { lat: number; lng: number } }): void {
     this.latitude = event.coords.lat;
     this.longitude = event.coords.lng;
+    console.warn('long = ' + this.longitude + 'lat = ' + this.latitude);
   }
 
   private getUserLocation(): void {
